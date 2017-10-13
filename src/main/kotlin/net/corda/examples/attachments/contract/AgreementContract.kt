c2c3bd8e1f890d716f0b681b8aa2730b65337d6d
Roger Willis <roger.willis@r3cev.com>
2017-10-13 15:13:40 +0100

    Update AgreementContract.kt
    Just moving the constraint checks on to one line as its easier to read like that.

--- src/main/kotlin/net/corda/examples/attachments/contract/AgreementContract.kt
+++ src/main/kotlin/net/corda/examples/attachments/contract/AgreementContract.kt
@@ -17,21 +17,15 @@ open class AgreementContract : Contract {
 
     override fun verify(tx: LedgerTransaction) = requireThat {
         // Constraints on the inputs, outputs and commands.
-        "The transaction should have no inputs" using
-                (tx.inputs.isEmpty())
-        "The transaction should have an AgreementState output" using
-                (tx.outputsOfType<AgreementState>().size == 1)
-        "The transaction should have no other outputs" using
-                (tx.outputs.size == 1)
-        "The transaction should have an Agree command" using
-                (tx.commandsOfType<Commands.Agree>().size == 1)
-        "The transaction should have no other commands" using
-                (tx.commands.size == 1)
+        "The transaction should have no inputs" using (tx.inputs.isEmpty())
+        "The transaction should have an AgreementState output" using (tx.outputsOfType<AgreementState>().size == 1)
+        "The transaction should have no other outputs" using (tx.outputs.size == 1)
+        "The transaction should have an Agree command" using (tx.commandsOfType<Commands.Agree>().size == 1)
+        "The transaction should have no other commands" using (tx.commands.size == 1)
 
         // Constraints on the included attachments.
         val nonContractAttachments = tx.attachments.filter { it !is ContractAttachment }
-        "The transaction should have a single non-contract attachment" using
-                (nonContractAttachments.size == 1)
+        "The transaction should have a single non-contract attachment" using (nonContractAttachments.size == 1)
         val attachment = nonContractAttachments.single()
 
         // TODO: Switch to constraint on the jar's signer.
@@ -40,7 +34,7 @@ open class AgreementContract : Contract {
         "The jar's hash should be correct" using (attachment.id == BLACKLIST_JAR_HASH)
 
         val attachmentJar = attachment.openAsJAR()
-        "The attachment jar's first entry should be entitled blacklist.txt" using
+        "The attachment jar's first entry should be entitled blacklist.txt" using 
                 (attachmentJar.nextEntry.name == "blacklist.txt")
 
         // Constraints on the blacklisted parties.
@@ -49,14 +43,12 @@ open class AgreementContract : Contract {
         val participants = agreement.participants
         val participantsOrgs = participants.map { it.name.organisation }
         val overlap = blacklistedCompanies.toSet().intersect(participantsOrgs)
-        "The agreement involved blacklisted parties: $overlap" using
-                (overlap.isEmpty())
+        "The agreement involved blacklisted parties: $overlap" using (overlap.isEmpty())
 
         // Constraints on the signers.
         val command = tx.commands.single()
         val participantKeys = participants.map { it.owningKey }
-        "All the parties to the agreement are signers" using
-                (command.signers.containsAll(participantKeys))
+        "All the parties to the agreement are signers" using (command.signers.containsAll(participantKeys))
     }
 
     interface Commands {
